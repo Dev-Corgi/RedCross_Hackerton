2c0d4e669988bab81531e097790ddaf7206e62e0
Dev-Corgi <bora5242@gmail.com>
2023-08-02 15:57:34 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Eat <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzess!</p>
         </a>
