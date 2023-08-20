53df0da67cb75e92ae87088b562bfe1576b1b384
Dev-Corgi <bora5242@gmail.com>
2023-08-20 15:14:07 +0900

    temp

---
+++ Components/Div/CorgiDiv.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+export default function CorgiDiv(props) {
+  const [currentMotions, setCurrentMotions] = useState([]);
+
+  useEffect(() => {
+    props.motions.forEach((motionset) => {
+      const input = motionset.input;
+      const motion = motionset.motion;
+      const condition = motionset.condition
+
+      const handleMotion = (event) => {
+        if(condition == null || (condition * input.event.detail.state>0)){
+        setCurrentMotions((prevState) => {
+          const updatedMotions = [...prevState, motion.handleMotion(event.detail.state)];
+          return updatedMotions;
+        });
+      }
+      };
+
+      if (input != null) {
+        input.eventTarget.addEventListener(input.eventname, handleMotion);
+      } else if (input == null) {
+        setCurrentMotions((prevState) => {
+          const updatedMotions = [...prevState, motion.handleMotion(1)];
+          return updatedMotions;
+        });
+      }
+    });
+  }, []);
+
+  // JSX를 반환하는 함수를 정의합니다.
+  const renderMotionDiv = () => {
+    const valuelist = {};
+    const transitionlist = {};
+
+    currentMotions.forEach((motionset) => {
+      Object.keys(motionset).forEach((key) => {
+        if (key !== "transition") {
+          valuelist[key] = motionset[key];
+          transitionlist[key] = motionset["transition"];
+        }
+      });
+    });
+
+    // "motion.div"의 속성으로 valuelist와 transitionlist를 사용합니다.
+    return (
+      <motion.div
+        className={props.classname}
+        animate={Object.keys(valuelist).length === 0 ? {} : valuelist}
+        transition={Object.keys(transitionlist).length === 0 ? {} : transitionlist}
+      >
+        {props.children}
+      </motion.div>
+    );
+  };
+
+  // 반환된 JSX를 렌더링합니다.
+  return <>{renderMotionDiv()}</>;
+}
---
+++ roll/Working_intro.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import ControllerSet from "../Controller/ControllerSet"; // Update the import statement here
+import CorgiDiv from "../Components/Div/CorgiDiv";
+import MovingMotionStep from "../Motion/MovingMotionStep";
+export default function Intro(props) {
+  const [showModal, setShowModal] = useState(false);
+
+
+  return (
+    <div className="w-full h-[100vh] relative overflow-hidden">
+
+      <CorgiDiv
+      classname="w-full h-screen absolute left-0 top-0"
+      motions = {[ 
+        {input:  ControllerSet.getValue("globalscroll"), motion : new MovingMotionStep(0,70,"vh",1,0,1.5,[0.645, 0.045, 0.355, 1.000])}
+       ]}>
+      <img
+          src="/img/tempbackground.png"
+        />
+        </CorgiDiv>
+
+     <CorgiDiv
+     classname = "w-full h-[200vh] relative" 
+     motions = {[ 
+      {input:  ControllerSet.getValue("globalscroll"), motion : new MovingMotionStep(0,100,"vh",1,0,1.5,[0.645, 0.045, 0.355, 1.000])}
+     ]}>
+      <div className="w-full h-[100vh] absolute left-0 top-0">
+
+        <div className="bg-[rgba(0,0,0,0.10)] w-full h-screen absolute left-0 top-0 overflow-hidden">
+          <div
+            className="text-[#ffffff] text-left absolute left-[calc(50%_-_124px)] top-[440px]"
+            style={{ font: "700 60px 'Pretendard', sans-serif" }}
+          >
+            Discover
+          </div>
+        </div>
+
+        <button onClick={() => setShowModal(true)} className="absolute left-[calc(50%-100px)] top-2/3 px-6 py-3 rounded bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-gray-100 tracking-wider drop-shadow-xl">
+                            Make your Itinerary
+                        </button>
+      </div>
+
+      <div className="w-full h-[100vh] absolute left-0 top-[100vh]">
+        <div
+          className="w-full h-[108vh] absolute left-0 bottom-0 overflow-hidden"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%,rgba(0, 0, 0, 1.00) 8.159700781106949%)",
+          }}
+        >
+          <img
+            className="w-[497px] h-[606px] absolute left-[calc(50%_-_-53px)] top-80 overflow-hidden"
+            src="/img/pic_introbackground.png"
+          />
+
+          <div
+            className="text-[#ffffff] text-left absolute left-[calc(50%_-_496px)] top-[347px]"
+            style={{ font: "400 36px 'Pretendard', sans-serif" }}
+          >
+            PlaceHolder
+          </div>
+
+          <div
+            className="text-[#ffffff] text-left absolute left-[calc(50%_-_496px)] top-[437px] w-[367px]"
+            style={{ font: "400 16px 'Pretendard', sans-serif" }}
+          >
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot;
+            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
+            book is a treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor
+            sit amet..&quot;, comes from a line in section 1.10.32.
+            <br />
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+            below for those interested. Sections 1.10.32 and 1.10.33 from
+            &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also
+            reproduced in their exact original form, accompanied by English
+            versions from the 1914 translation by H. Rackham.
+          </div>
+        </div>
+      </div>
+      </CorgiDiv>
+
+      <QuestionModal show={showModal} handleClose={() => setShowModal(false)} />
+
+      <FloatingButton></FloatingButton>
+    </div>
+
+
+  );
+}
